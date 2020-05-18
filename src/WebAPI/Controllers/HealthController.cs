c04190109581b0f849889c50e6775670ee1f3eee
Sergei Antonov <Sergei_Antonov2@epam.com>
2020-05-18 14:51:52 +0400

    Initial project structure

---
+++ src/WebAPI/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [Route("Health")]
+        [HttpGet]
+        public IActionResult CheckHealth()
+        {
+            return this.Ok("The service is available.");
+        }
+    }
+}
